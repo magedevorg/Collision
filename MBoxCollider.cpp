748f07c8c94cf195bfe8be39522511f9a0da5daa
magedev84 <UltraHM2@gmail.com>
2024-03-17 01:31:09 +0900

    ...

---
+++ MBoxCollider.cpp
@@ -0,0 +1,7 @@
+#include "MBoxCollider.h"
+
+
+MBoxCollider::MBoxCollider(const MVector3& inCenter, const MVector3& inExtent)
+{
+
+}
---
+++ MBoxCollider.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include "MVector.h"
+
+
+class MBoxCollider
+{
+public:
+	MBoxCollider(){}
+	MBoxCollider(const MVector3& inCenter, const MVector3& inExtent);
+
+
+protected:
+	
+};
