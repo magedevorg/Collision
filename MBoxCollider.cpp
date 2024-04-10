7bf2dea339bb51d88bc0d1c081bdaf907178c353
magedev84 <UltraHM2@gmail.com>
2024-04-10 10:41:05 +0900

    ...

--- MBoxCollider.cpp
+++ MBoxCollider.cpp
@@ -1,7 +1,55 @@
 #include "MBoxCollider.h"
 
 
-MBoxCollider::MBoxCollider(const MVector3& inCenter, const MVector3& inExtent)
-{
 
+MBoxCollider::MBoxCollider()
+	: Position(MVector3::Zero)
+	, Size(MVector3::Zero)
+{
+}
+
+
+MBoxCollider::MBoxCollider(const MVector3& inPosition, const MVector3& inSize)
+{
+	Set(inPosition, inSize);
+}
+
+
+
+void MBoxCollider::Set(const MVector3& inPosition, const MVector3& inSize)
+{
+	Position = inPosition;
+	Size = inSize;
+
+	Update();
+}
+
+void MBoxCollider::SetPosition(const MVector3& inPosition)
+{
+	Position = inPosition;
+	Update();
+}
+
+void MBoxCollider::SetSize(const MVector3& inSize)
+{
+	Size = inSize;
+	Update();
+}
+
+
+void MBoxCollider::Update()
+{
+	// 기본 위치
+	Vertices[0] = Position - Size;
+	
+	// 
+	Vertices[1] = Vertices[0];
+	Vertices[1].X += (Size.X * 2);
+
+	// 
+	Vertices[2] = Vertices[0];
+	Vertices[2].Y += (Size.Y * 2);
+
+	Vertices[3] = Vertices[0];
+	Vertices[3].Z += (Size.Z * 2);
 }
--- MBoxCollider.h
+++ MBoxCollider.h
@@ -7,10 +7,31 @@
 class MBoxCollider
 {
 public:
-	MBoxCollider(){}
-	MBoxCollider(const MVector3& inCenter, const MVector3& inExtent);
+	MBoxCollider();
+	MBoxCollider(const MVector3& inPosition, const MVector3& inSize);
 
+public:
+	// 설정
+	void Set(const MVector3& inPosition, const MVector3& inSize);
+
+	void SetPosition(const MVector3& inPosition);
+	void SetSize(const MVector3& inSize);
+
+	// 
+	const std::array<MVector3, 4>& GetVertices() const {
+		return Vertices;
+	}
 
 protected:
-	
+	void Update();
+
+protected:
+	// 위치
+	MVector3 Position;
+
+	// 크기
+	MVector3 Size;
+
+	// 라인 리스트
+	std::array<MVector3, 4> Vertices;
 };
